75a0f43a16fcd50ccbac06e5dbfddc9677b5dc7e
Jay Mistry <jayu412@gmail.com>
2020-04-21 21:51:15 +0530

    - SharedSettings.json file added to use multiple configuration file in project. - NLog library added. - NLog, logs to database added. - Logs added when application starts. - StockItem table few properties type changed.

--- DotNetCore.API/Migrations/20190823065500_Warehouse.Designer.cs
+++ DotNetCore.API/Migrations/20190823065500_Warehouse.Designer.cs
@@ -88,8 +88,7 @@ namespace DotNetCore.API.Migrations
 
                     b.Property<string>("Tags")
                         .ValueGeneratedOnAddOrUpdate()
-                        .HasColumnType("nvarchar(max)")
-                        .HasComputedColumnSql("json_query([CustomFields],N'$.Tags')");
+                        .HasColumnType("nvarchar(max)");
 
                     b.Property<decimal?>("TaxRate")
                         .IsRequired()
